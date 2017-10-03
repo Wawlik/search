6c5de992273bb332c36ce2ff68e24ee91e482e3c
wawlik <craazzy777@gmail.com>
2017-10-03 21:41:27 +0300

    main files of project

---
+++ components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="Search">
+			<input type="text" class="Search__input" v-model="query" placeholder="Type to search in Wikipedia" />
+		<button class="Search_button" @click="search()">Найти</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'search',
+		data() {
+			return {
+				query: ''
+			}
+		},
+		methods: {
+			search()
+{
+	this.$store.dispatch('search', this.query)
+}		}
+	}
+</script>
